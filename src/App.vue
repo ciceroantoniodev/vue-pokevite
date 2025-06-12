f14a21931df8b8f42572a6480604aa79f10ae528
Cícero Antônio da Silva <ciceroantonio.dev@gmail.com>
2025-06-12 17:20:44 -0300

    Renderização da tela inicial

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup>
+  import { ref } from 'vue'
 
+  let num = ref(0)
 </script>
 
 <template>
-  <h1>Hello Word!</h1>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">
+        <img src="https://cdn-icons-png.flaticon.com/512/188/188987.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        PokeVite
+      </RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link" aria-current="page" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/about">About</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="main">
+    <RouterView />
+  </div>
+
+  <footer class="bg-primary text-light">
+    <div>
+      Design in {{ new Date().getFullYear() }}
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-
+  footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 56px;
+  }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+        <br><br><br><br><br><br>
+        <h1>Home</h1>
+    </main>
+</template>
